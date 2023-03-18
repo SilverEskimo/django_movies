a4780b2b61c51715a9298592f0bd02486a6ea3c0
SilverEskimo <slavasereb@gmail.com>
2023-03-18 20:39:51 +0200

    commit django1

---
+++ query_db.py
@@ -0,0 +1,62 @@
+from update_tables import *
+from movies_app.models import *
+
+
+if __name__ == '__main__':
+    # q1 = Movie.objects.filter(year__gt=1990)
+    # print(q1)
+    #
+    # q2 = Movie.objects.filter(year__lt=2000, name__icontains="sky")
+    # print(q2)
+
+    # Add review text “GREAT MOVIE” to reviews with rating higher than 9.4
+    # q3 = Review.objects.filter(rating__gte=9).update(review_text="GREAT MOVIE")
+    # print(Review.objects.filter(
+    #     review_text="GREAT MOVIE"
+    #     )
+    # )
+
+    # Get all the reviews for movie “pulp fiction”
+    # q4 = Review.objects.filter(movie__name__icontains="pulp fiction")
+    # print(q4)
+
+    # Add new review with rating 10 and text “wow” to all the movies that contain “god father” in their name
+    # q5 = Movie.objects.filter(name__icontains="godfather")
+    # for movie in q5:
+    #     create_review(10, datetime.date.today(), movie, "incredible!")
+
+    # Add the following durations to the movies:
+        # Interstellar - 2h 49min
+        # The Godfather - 2h 55min
+        # Scarface - 2h 50 min
+    # movies_to_update = {
+    #     "Interstellar": 169,
+    #     "The Godfather": 175,
+    #     "Scarface": 170
+    # }
+    #
+    # for k, v in movies_to_update.items():
+    #     Movie.objects.filter(name=k).update(duration_in_min=v)
+
+    # Get all the movies that last more than 2h 50min
+    # print(Movie.objects.filter(duration_in_min__gt=170))
+
+    # Get all the movies with duration longer than 120 min and release year before 2000
+    # print(Movie.objects.filter(duration_in_min__gt=120, year__lt=2000))
+
+    # Get all the movies with rating higher than 9.4
+    movies = Movie.objects.prefetch_related('review_set').all()
+    for m in movies:
+        res = m.review_set.filter(rating__gt=9.4)
+        if res:
+            avg = sum(res.rating)/len(res)
+            if avg >= 9.4:
+                print(f"Movie Name: {m.name}\nRatings:")
+                print(*res, sep="\n")
+
+    # Get 5 movies with highest rating (hint: look in django documentation how to run order by queries)
+
+
+
+
+
---
+++ update_tables.py
@@ -0,0 +1,59 @@
+import csv
+import datetime
+import os
+import django
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "movies.settings"
+django.setup()
+
+from movies_app.models import *
+import movies_app.models
+
+
+def create_movie(movie_name:str, movie_year: int, director: movies_app.models.Director=None, movie_description=None):
+    movie = Movie.objects.create(
+        name=movie_name,
+        year=movie_year,
+        director=director,
+        description=movie_description
+    )
+    return movie
+
+
+def create_director(director_name:str, birth_date: datetime.date):
+    director = Director.objects.create(
+        name=director_name,
+        birth_date=birth_date
+    )
+    return director
+
+
+def create_review(rating: float, review_date: datetime.date, movie: Movie, review_text=None):
+    review = Review(
+        rating=rating,
+        review_date=review_date,
+        movie=movie,
+        review_text=review_text
+    )
+    try:
+        review.full_clean()
+        review.save()
+        return review
+    except Exception as e:
+        print(e)
+
+
+def insert_movies(file_name: str):
+    with open(f"{file_name}.csv", "r") as f:
+        csv_reader = csv.DictReader(f)
+        list_of_movies = list(csv_reader)
+        for movie in list_of_movies:
+            m = create_movie(
+                movie["Movie_Name"],
+                int(movie["Release_date"])
+            )
+            create_review(float(movie["Rating"]), datetime.date.today(), m)
+
+
+if __name__ == '__main__':
+    insert_movies("IMDB")
